86e675f040b66ed9a90920746312abf48c125575
t.VenkatSai <129149218+venkatsait33@users.noreply.github.com>
2026-08-08 12:14:17 +0530

    docker update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,9 @@ set -e
 
 export ASPNETCORE_URLS="http://+:${PORT:-10000}"
 
+# Disable config file-watching (FileSystemWatcher). Render's containers have
+# a low inotify instance limit, and hot-reload of appsettings.json isn't
+# needed in production anyway - this avoids the IOException crash on boot.
+export DOTNET_hostBuilder__reloadConfigOnChange="false"
+
 exec dotnet IForm.Web.dll
